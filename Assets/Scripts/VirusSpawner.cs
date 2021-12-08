0ed6f739dd6623fd9e9c5c2494ccb263503b0fb5
Eduardo Moraes <65173584+Fkscgy@users.noreply.github.com>
2021-12-07 21:54:27 -0300

    Iniciado as animações virus

--- Assets/Scripts/VirusSpawner.cs
+++ Assets/Scripts/VirusSpawner.cs
@@ -7,7 +7,7 @@ public class VirusSpawner : MonoBehaviour
     [SerializeField]
     GameObject p1,p2;
     [SerializeField]
-    GameObject virus;
+    GameObject[] virus;
     [SerializeField]
     float maxTime,minTime,decrease;
     float time;
@@ -22,7 +22,7 @@ public class VirusSpawner : MonoBehaviour
         while(true)
         {
             yield return new WaitForSeconds(time);
-            Instantiate(virus,new Vector3(Random.Range(p1.transform.position.x,p2.transform.position.x),p1.transform.position.y),Quaternion.identity);
+            Instantiate(virus[Random.Range(0,2)],new Vector3(Random.Range(p1.transform.position.x,p2.transform.position.x),p1.transform.position.y),Quaternion.identity);
             if(time>minTime)
             {
                 time = maxTime-decrease;
